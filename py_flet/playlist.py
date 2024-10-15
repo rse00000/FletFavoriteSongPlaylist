33229926221fbe829f89c89ea444d871411a571f
rse00000 <sryouma060215@gmail.com>
2024-10-15 22:02:25 +0900

    spotifyのプレイリストを閲覧するflet練習用アプリ

---
+++ py_flet/playlist.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "My Favorite Music"
+    page.theme = ft.Theme(
+        color_scheme_seed=ft.colors.PINK
+    )
+    img = ft.Image(
+        src="https://storage.googleapis.com/spotifynewsroom-jp.appspot.com/1/2020/12/Spotify_Icon_RGB_Green.png",
+        width=100,
+        height=100,
+    )
+    row = ft.Row(
+        controls=[
+            img,
+            ft.FilledButton(text="好きな曲のプレイリスト", url="https://open.spotify.com/playlist/0Xq3Nvbh5mkcTAbv73v0nt"),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
+    page.add(
+        row,
+    )
+
+
+ft.app(main)
